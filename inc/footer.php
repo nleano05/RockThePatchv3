31d6948e5933f08021f6952a8930363fa8bfc160
Patches <isuPatches@yahoo.com>
2015-07-12 21:11:10 -0500

    New social media icons and rss/footer CSS work
    #3

--- inc/footer.php
+++ inc/footer.php
@@ -25,53 +25,59 @@ log_util::log(LOG_LEVEL_DEBUG, "currentURL: " . $currentURL);
 ?>
 <div id="social-media">
     <!--suppress HtmlUnknownTarget -->
-    <a href="aim:addbuddy?screenname=isuPatches"><img src="/images/social-media/aim.png" alt="Add Patches on AIM"
+    <a href="aim:addbuddy?screenname=isuPatches"><img src="/images/social-media/aim.png"
+                                                      class="aim-icon"
+                                                      alt="Add Patches on AIM"
                                                       title="Add Patches on AIM"/></a>
     <a href="https://www.facebook.com/isuPatches"><img src="/images/social-media/facebook.png"
+                                                       class="facebook-icon"
                                                        alt="Add Patches on Facebook"
                                                        title="Add Patches on Facebook"/></a>
     <a href="https://www.facebook.com/rockthepatch"><img src="/images/social-media/facebook-alt.png"
+                                                         class="facebook-alt-icon"
                                                          alt="Like Patches' Facebook Fan Page"
                                                          title="Like Patches' Facebook Fan Page"/></a>
-    <a href="https://github.com/isuPatches"><img src="/images/social-media/github.png" alt="Follow Patches on GitHub"
-                                                 title="Follow Patches on GitHub" style="width:35px;height:auto;"/></a>
-    <a href="https://plus.google.com/u/0/+SarahKlinefelter89/posts"><img src="/images/social-media/google.png"
+    <a href="https://github.com/isuPatches"><img src="/images/social-media/github.png"
+                                                 class="github-icon"
+                                                 alt="Follow Patches on GitHub"
+                                                 title="Follow Patches on GitHub"/></a>
+    <a href="https://plus.google.com/u/0/+SarahKlinefelter89/posts"><img src="/images/social-media/google-plus.png"
+                                                                         class="google-plus-icon"
                                                                          alt="Add Patches on Google+"
                                                                          title="Add Patches on Google+"/></a>
-    <a href="https://plus.google.com/u/0/+Rockthepatch/posts"><img src="/images/social-media/google-fan.png"
-                                                                   alt="Follow Patches' Google+ Fan Page"
-                                                                   title="Follow Patches' Google+ Fan Page"/></a>
     <a href="http://pinterest.com/isuPatches/"><img src="/images/social-media/pinterest.png"
-                                                    alt="Follow Patches on Pintrest" title="Follow Patches on Pintrest"
-                                                    style="width:28px;height:auto;"/></a>
-    <a href="http://www.last.fm/user/isuPatches"><img src="/images/social-media/lastfm.png" alt="Add Patches on Last FM"
+                                                    class="pinterest-icon"
+                                                    alt="Follow Patches on Pintrest"
+                                                    title="Follow Patches on Pintrest"/></a>
+    <a href="http://www.last.fm/user/isuPatches"><img src="/images/social-media/lastfm.png"
+                                                      class="lastfm-icon"
+                                                      alt="Add Patches on Last FM"
                                                       title="Add Patches on Last FM"/></a>
-    <a href="https://www.linkedin.com/pub/sarah-klinefelter/a8/b35/979"><img src="/images/social-media/linkedIN.png"
+    <a href="https://www.linkedin.com/pub/sarah-klinefelter/a8/b35/979"><img src="/images/social-media/linkedin.png"
+                                                                             class="linkedin-icon"
                                                                              alt="Connect with Patches on LinkedIN"
                                                                              title="Connect with Patches on LinkedIN"/></a>
     <a href="http://www.pandora.com/profile/isupatches"><img src="/images/social-media/pandora.png"
+                                                             class="pandora-icon"
                                                              alt="Add Patches on Pandora Radio"
                                                              title="Add Patches on Pandora Radio"/></a>
     <a href="https://www.rockthepatch.com/rss/current-events.xml" type="application/rss+xml"><img
-            src="/images/social-media/rss.png" alt="View or Subscribe to Patches' RSS Feed" title="Patches' RSS Feed"/></a>
+            src="/images/icons-and-logos/rss.png" class="rss-icon" alt="View or Subscribe to Patches' RSS Feed" title="Patches' RSS Feed"/></a>
     <a href="https://soundcloud.com/isupatches"><img src="/images/social-media/soundcloud.png"
+                                                     class="soundcloud-icon"
                                                      alt="Follow Patches on SoundCloud"
                                                      title="Follow Patches on SoundCloud"/></a>
     <a href="http://twitter.com/#!/isuPatches"><img src="/images/social-media/twitter.png"
                                                     alt="Follow Patches on Twitter" title="Follow Patches on Twitter"/></a>
-    <a href="http://pulse.yahoo.com/_FWIZRSMLF3GEXRTMKJKO3TDUVY"><img src="/images/social-media/yahoo.png"
-                                                                      alt="Add Patches on Yahoo"
-                                                                      title="Add Patches on Yahoo"/></a>
     <!--suppress HtmlUnknownTarget -->
-    <a href="ymsgr:addfriend?isuPatches"><img src="/images/social-media/yahoomessenger.png"
+    <a href="ymsgr:addfriend?isuPatches"><img src="/images/social-media/yahoo.png"
+                                              class="yahoo-icon"
                                               alt="Add Patches on Yahoo Messenger"
                                               title="Add Patches on Yahoo Messenger"/></a>
     <a href="http://www.youtube.com/isuPatches"><img src="/images/social-media/youtube.png"
-                                                     alt="Subscribe to Patches on YouTube (current)"
-                                                     title="Subscribe to Patches on YouTube (current)"/></a>
-    <a href="http://www.youtube.com/ccPatches"><img src="/images/social-media/youtubealt.png"
-                                                    alt="Subscribe to Patches on YouTube (old)"
-                                                    title="Subscribe to Patches on YouTube (old)"/></a>
+                                                     class="youtube-icon"
+                                                     alt="Subscribe to Patches on YouTube"
+                                                     title="Subscribe to Patches on YouTube"/></a>
 </div>
 <div id="note">
     <p><em><strong>*NOTE*</strong> You must have the respective software installed on your computer to add/follow
@@ -97,35 +103,10 @@ log_util::log(LOG_LEVEL_DEBUG, "currentURL: " . $currentURL);
         </p>
     </div>
 </div>
-<!--<div id="validation">-->
-<!--    <p>-->
-<!--        <a href="http://validator.w3.org/check?uri=--><?php //echo($currentURL); ?><!--&amp;ss=1"-->
-<!--           title="Check validation of this markup with HTML5 standards"><img src="/images/html5.png"-->
-<!--                                                                             id="html5-validation-link"-->
-<!--                                                                             alt="Check validation of this markup with HTML5 standards"-->
-<!--                                                                             class="html5"/></a>-->
-<!--        <a href="http://validator.w3.org/feed/check.cgi?url=https%3A//www.rockthepatch.com/rss/current-events.xml"-->
-<!--           title="Check validation of the RSS Feed"><img src="/images/valid-rss.png"-->
-<!--                                                         alt="Check validation of the RSS Feed" id="rss-validation-link"-->
-<!--                                                         class="valid"/></a>-->
-<!---->
-<!--        <a href="http://jigsaw.w3.org/css-validator/validator?uri=--><?php //echo($currentURLForCSS); ?><!--&amp;profile=css3"-->
-<!--           title="Check validation of CSS stylesheets with CSS3 standards"><img src="/images/valid-css.png"-->
-<!--                                                                                id="css-validation-link"-->
-<!--                                                                                alt="Check validation of CSS stylesheets with CSS3 standards"-->
-<!--                                                                                class="valid"/></a>-->
-<!---->
-<!--        <a href="http://validator.w3.org/check?uri=--><?php //echo($currentURL); ?><!--&amp;doctype=XHTML+1.1&amp;=1"-->
-<!--           title="Check validation of this markup with XHTML 1.1 standards"><img src="/images/valid-xhtml11.png"-->
-<!--                                                                                 id="xhtml-validation-link"-->
-<!--                                                                                 alt="Check validation of this markup with XHTML 1.1 standards"-->
-<!--                                                                                 class="valid"/></a>-->
-<!--    </p>-->
-<!--</div>-->
 <div class="clear"></div>
 <div id="contact-info">
     <p>
-        <strong>Created by:</strong> Patches,<br/> <a href="mailto:<?php if (isset($gMasterAdminEmail)) {
+        <strong>Created by:</strong> Patches<br/> <a href="mailto:<?php if (isset($gMasterAdminEmail)) {
             echo($gMasterAdminEmail);
         } ?>?subject='Rock%20the%20Patch!'%20Email%20from%20user" title="Email <?php if (isset($gMasterAdminName)) {
             echo($gMasterAdminName);
@@ -135,3 +116,5 @@ log_util::log(LOG_LEVEL_DEBUG, "currentURL: " . $currentURL);
         <strong>Updated:</strong> <?php echo($timeModified . " GMT"); ?>
     </p>
 </div>
+<div id="v3"><p>v3</p></div>
+<div class="clear"></div>
--- inc/rss-secondary.php
+++ inc/rss-secondary.php
@@ -1,8 +1,11 @@
 <h1>Upcoming 'Rock the Patch!' Events</h1>
-<?php														
-	lib::displayRSS(1, "../rss/current-events.xml");
+<?php
+lib::displayRSS(1, "../rss/current-events.xml");
 ?>
-
-<p class="rss-click"><em>Click on the Icon to see more of the RSS Feed</em></p>
-<a href="../rss/current-events.xml" type="application/rss+xml"><img src="/images/icons-and-logos/rss.png" alt="RSS Icon" title="View to Patches' RSS Feed" class="rss-icon" /></a>
+<p class="rss-click">
+    <a href="../rss/current-events.xml" type="application/rss+xml"><img src="/images/icons-and-logos/rss.png"
+                                                                        alt="RSS Icon"
+                                                                        title="View to Patches' RSS Feed"
+                                                                        class="rss-icon"/></a>
+</p>
 <div class="clear"></div>
